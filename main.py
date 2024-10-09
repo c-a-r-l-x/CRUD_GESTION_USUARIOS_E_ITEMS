d02a92a2f89a60bf1f3393905bce0569e57f6c3f
c-a-r-l-x <dev.carlx@gmail.com>
2024-10-09 13:54:47 -0300

    se crea método editar_usuario()

--- main.py
+++ main.py
@@ -178,8 +178,62 @@ def listar_usuarios_por_rol():
         finally:
             conexion.close()
 
+def editar_usuario():
+    conexion = conectar()
+    if conexion:
+        try:
+            username = input("Ingrese el nombre de usuario que desea editar: ")
+
+            cursor = conexion.cursor()
+            cursor.execute("SELECT * FROM usuarios WHERE username = ?", (username,))
+            usuario = cursor.fetchone()
+
+            if not usuario:
+                print(f"Usuario '{username}' no encontrado.")
+                return
+
+            nuevo_username = input("Nuevo nombre de usuario (opcional): ")
+            nuevo_email = input("Nuevo email (opcional): ")
+            nuevo_rol = input("Nuevo rol (1 para Usuario General, 2 para Administrador, opcional): ")
+
+            # actualizar username, si se ingresa uno nuevo:
+            if nuevo_username:
+                if validar_username(nuevo_username):
+                    cursor.execute("UPDATE usuarios SET username = ? WHERE username = ?", (nuevo_username, username))
+                    username = nuevo_username # actualizar username para futuras referencias.
+                else:
+                    print("El nombre de usuario debe tener entre 5 y 20 caracteres.")
+                    return
+            
+            # actualizar email, si se ingresa uno nuevo:
+            if nuevo_email:
+                if validar_email(nuevo_email):
+                    cursor.execute("UPDATE usuarios SET email = ? WHERE username = ?", (nuevo_email, username))
+                else:
+                    print("Formato de email no válido.")
+                    return
+            
+            # actualizar rol, si se ingresa uno nuevo:
+            if nuevo_rol:
+                if nuevo_rol == '1':
+                    nuevo_rol_id = 1
+                elif nuevo_rol == '2':
+                    nuevo_rol_id = 2
+                else:
+                    print("Opción de rol no válida. Solo se permite 1 (Usuario General) o 2 (Administrador).")
+                    return
+                cursor.execute("UPDATE usuarios SET role_id = ? WHERE username = ?", (nuevo_rol_id, username))
+            
+            conexion.commit()
+            print(f"Usuario editado con éxito.")
+        except sqlite3.Error as e:
+            print(f"Error al editar usuario: {e}.")
+        finally:
+            conexion.close()
+
 crear_tablas()
 registrar_usuario()
 iniciar_sesion()
 listar_usuarios()
 listar_usuarios_por_rol()
+editar_usuario()
